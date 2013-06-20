ad6b56462c082f4c66a80b5a39f36d9882074ace
Adam Chu <adamhchu@gmail.com>
2013-06-19 21:58:36 -0500

    email mostly done

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,9 +6,10 @@ class ContactController < ApplicationController
 
 	def create
 		@message = Message.new(params[:message])
+		@user = User.find(current_user.id)
 
 		if @message.valid?
-			UserMailer.new_message(@message).deliver
+			UserMailer.new_message(@message, @user).deliver
 			redirect_to('/main', :notice => "Message was successfully sent.")
 		else
 			flash.now.alert = "Please fill all fields."
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,10 +6,11 @@ default :from => "noreply@theharvardlist.com", :content_type => "text/html"
   		mail(:to => user.email, :subject => "Thanks for signing up!", :from => "notifications@theharvardlist.com")
   end
 
-  def new_message(message) #http://matharvard.ca/posts/2011/aug/22/contact-form-in-rails-3/
+  def new_message(message, user) #http://matharvard.ca/posts/2011/aug/22/contact-form-in-rails-3/
   	@message = message
+  	@user = user
   	mail(:subject => "[NapList.tld] #{message.subject}")
-  	mail(:to => message.email)
+  	mail(:to => @user.email)
   end
 
 end
