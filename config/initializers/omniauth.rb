16415a0cc734d3dd1292151718256fa07f972676
Adam Chu <adamhchu@gmail.com>
2013-06-30 20:50:33 -0500

    edited user profiles

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['51353934	2013567'], ENV['e040ba5311c9e2afe69bc8cd128ff3b6']
+  provider :facebook, "513539342013567", "e040ba5311c9e2afe69bc8cd128ff3b6"
 end
